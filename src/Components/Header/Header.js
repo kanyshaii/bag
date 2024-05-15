ba0232910fef7a52d1cf14c6691518c67c0c4e4a
kanyshaii <kanyshaibaktybekova07@gmail.com>
2024-05-15 21:33:16 +0600

    finished making the  component Logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,12 +2,14 @@ import './Header.css';
 import Menu from '../Menu/Menu';
 import Button from '../Button/Button';
 import {Link} from 'react-router-dom'
+import Logo from '../Logo/Logo';
 
 function Header() {
   return (
     <header className="Header">
       <div className="container">
         <div className="Head flex">
+          <Logo/>
           <Menu />
           <div className="Buttons flex">
             <Button>Login</Button>
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './Logo.css';
+import logo from '../../img/Icons/logo.svg'
 
 function Logo() {
   return (
-    <>
-     
-    </>
+    <Link to ='/' className='Logo'>
+      <img src={logo} alt="" />
+      <span>Fashion</span>
+    </Link>
   );
 }
 
